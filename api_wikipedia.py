3eda1dfb0c6a333cd04c830b750d1316098ab90a
Eric Moreira <ericmoreira@Mac-Lykos.local>
2023-04-06 16:02:31 -0300

    Wikipedia Summarize project using Jaro-Winkler Similarity algorithm to find the best match

---
+++ api_wikipedia.py
@@ -0,0 +1,58 @@
+import wikipedia
+import nltk
+import json
+from nltk.metrics.distance import jaro_winkler_similarity
+
+wikipedia.set_lang("pt")
+nltk.download('punkt')
+
+# Find the best match using the Jaro-Winkler similarity algorithm
+def find_best_match(input_str, options_list):
+    best_match = None
+    best_score = 0
+    for option in options_list:
+        score = jaro_winkler_similarity(input_str.lower(), option.lower())
+        if score > best_score:
+            best_score = score
+            best_match = option
+    return best_match
+
+
+def get_wiki_info(topic):
+
+    results = wikipedia.search(topic)
+    # Comente o resto da função e descomente a linha abaixo para testar
+    #return json.dumps(results)
+#'''
+    # Pega o texto do resumo da página da Wikipedia
+    #summary = wikipedia.summary(topic, sentences=2)
+    best_match = find_best_match(topic, results)
+    summary = wikipedia.summary(find_best_match(topic, results), sentences=5)
+
+    # Pega os links relacionados à página da Wikipedia
+    related_links = wikipedia.page(topic).links
+
+    # Cria um dicionário com as informações obtidas
+    wiki_info = {
+        "best_match": best_match,
+        "summary": summary,
+        "results": results
+    }
+
+    # Converte o dicionário para um objeto JSON e retorna
+    return json.dumps(wiki_info)
+#'''
+def main():
+    # Pergunta ao usuário o tópico que deseja pesquisar
+    topic = input("Qual tópico você deseja pesquisar na Wikipedia? ")
+
+    # Obtém as informações da Wikipedia e imprime como JSON
+    wiki_info = get_wiki_info(topic)
+    results = json.loads(wiki_info)
+    print(f"O melhor resultado que encontramos foi: {results.get('best_match')}\n")
+    print(f"{results.get('summary')}\n")
+    print(f"Outros resultados: {results.get('results')}")
+
+
+if __name__ == '__main__':
+    main()
